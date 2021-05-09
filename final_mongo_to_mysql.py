fd1de6ef4cb414f355ad4b9b0fcd69c0d1c69e4f
dranarucom <1327133922@qq.com>
2021-05-09 09:17:22 +0800

    修改mongo数据转mysql的逻辑，在mysql添加基因注释列，代码添加注释

--- final_mongo_to_mysql.py
+++ final_mongo_to_mysql.py
@@ -2,15 +2,15 @@ from pymongo import MongoClient
 import pymysql
 
 
-def common_mongo_to_mysql(ab_db_name: str, gene_db_name: str, sql_table_name: str):
-    if ab_db_name not in clint.list_database_names():
-        print("没有【{0}】这个库".format(ab_db_name))
-        return
+def common_mongo_to_mysql(gene_db_name: str, sql_table_name: str):
+    # 函数说明：
+    # 功能：将mongoDB中的基因和来源的文献储存到MySQL中
+    # 参数：ab_db_name：mongo中的摘要库；gene_db_name：mongo中的基因库；sql_table_name：储存数据的MySQL表
+    # 返回值：无
     if gene_db_name not in clint.list_database_names():
         print("没有【{0}】这个库".format(gene_db_name))
         return
 
-    ab_db = clint.get_database(ab_db_name)
     gene_db = clint.get_database(gene_db_name)
 
     sql_cursor = mysql_clint.cursor()
@@ -19,15 +19,15 @@ def common_mongo_to_mysql(ab_db_name: str, gene_db_name: str, sql_table_name: st
         AB TEXT,
         ORG_NAME VARCHAR(5),
         MEA VARCHAR(10),
-        GENE_NAME VARCHAR(25)
+        GENE_NAME VARCHAR(25),
+        ANNOTATION VARCHAR(100)
         """
     create_sql_table = "CREATE TABLE IF NOT EXISTS {0} ({1})".format(sql_table_name, sql_add_key)
     sql_cursor.execute(create_sql_table)
     # INSERT IGNORE INTO会忽略数据库中已经存在的数据，如果数据库没有数据，就插入新的数据，如果有数据的话就跳过这条数据。这样就可以保留数据库中已经存在数据，达到在间隙中插入数据的目的
-    insert_sql = "INSERT IGNORE INTO {0}(TITLE, AB, ORG_NAME, MEA, GENE_NAME) VALUES(%s, %s, %s, %s, %s)".format(sql_table_name)
+    insert_sql = "INSERT IGNORE INTO {0}(TITLE, AB, ORG_NAME, MEA, GENE_NAME, ANNOTATION) VALUES(%s, %s, %s, %s, %s, %s)".format(sql_table_name)
 
-    for cname in ab_db.list_collection_names():
-        ab_col = ab_db.get_collection(cname)
+    for cname in gene_db.list_collection_names():
         gene_col = gene_db.get_collection(cname)
         gene_cursor = gene_col.find({})
         for ref in gene_cursor:
@@ -35,15 +35,12 @@ def common_mongo_to_mysql(ab_db_name: str, gene_db_name: str, sql_table_name: st
             m = ref['指标']
             org = ref['物种']
             title = ref['标题']
-            r = ab_col.find_one({'标题': title, '指标': m, '物种': org})
-            if r:
-                ab = r['摘要']
-            else:
-                ab = "NULL"
+            ab = ref["摘要"]
+            anno = ref["注释"]
 
             try:
                 # 执行插入语句
-                sql_cursor.execute(insert_sql, [title, ab, org, m, gname])
+                sql_cursor.execute(insert_sql, [title, ab, org, m, gname, anno])
                 # 提交到数据库执行
                 mysql_clint.commit()
             except pymysql.Error:
@@ -54,6 +51,42 @@ def common_mongo_to_mysql(ab_db_name: str, gene_db_name: str, sql_table_name: st
     sql_cursor.close()
 
 
+def add_column_to_mysql(source_db: str, source_key: str, source_insert_key: str, target_sql_table: str):
+    # 函数说明：
+    # 功能：从mongo的一个库中提取一系列值插入到MySQL的一个表中作为新的一列
+    # 参数：source_db：mongo中的库；source_key：用于在mongo和MySQL之间标识同一个记录的值在mongo中对应的key；target_sql_table：要插入的MySQL表
+    # 返回值：无
+    if source_db in clint.list_database_names():
+        sdb = clint.get_database(source_db)
+    else:
+        print("mongo中没有【{0}】这个库".format(source_db))
+        return
+
+    sql_cursor = mysql_clint.cursor()
+    # 在GENE_NAME后面添加GENE_ANNOTATION这一列
+    # sql_add_column = "ALTER TABLE {0} ADD COLUMN GENE_ANNOTATION VARCHAR(100) AFTER GENE_NAME".format(target_sql_table)
+    # sql_cursor.execute(sql_add_column)
+    sql_update = 'UPDATE {0} SET GENE_ANNOTATION = %s WHERE GENE_NAME = %s AND ORG_NAME = %s'.format(target_sql_table)
+
+    for cname in sdb.list_collection_names():
+        scol = sdb.get_collection(cname)
+        cursor = scol.find({})
+        for ref in cursor:
+            gname = ref["gene_name"]
+            anno = ref["annotation"]
+            try:
+                # 执行插入语句
+                sql_cursor.execute(sql_update, (anno, gname, cname))
+                # 提交到数据库执行
+                mysql_clint.commit()
+            except pymysql.Error:
+                # 发生错误时回滚
+                mysql_clint.rollback()
+                print((anno, gname, cname))
+        cursor.close()
+    sql_cursor.close()
+
+
 clint = MongoClient()
 sql_host = "localhost"
 sql_port = 3306
@@ -61,11 +94,14 @@ sql_user = "root"
 sql_pwd = "root"
 sql_db_name = 'ycs_gene_db'
 mysql_clint = pymysql.connect(host=sql_host, port=sql_port, user=sql_user, password=sql_pwd, db=sql_db_name)
-ab_db_list = ["新知网摘要库", "新万方摘要库", "新维普摘要库"]
-gene_db_list = ["新知网基因库", "新万方基因库", "新维普基因库"]
-sql_table_list = ["知网", "万方", "维普"]
-ignore_list = ["知网"]
+""" sql_table_list = ["知网", "万方", "维普"]
+for i in range(0, 3):
+    add_column_to_mysql("新物种基因库", "annotation", "gene_name", sql_table_list[i]) """
+
+gene_db_list = ["新知网基因库2", "新万方基因库2", "新维普基因库2"]
+sql_table_list = ["知网2", "万方2", "维普2"]
+ignore_list = []
 for i in range(0, 3):
     if sql_table_list[i] in ignore_list:
         continue
-    common_mongo_to_mysql(ab_db_list[i], gene_db_list[i], sql_table_list[i])
+    common_mongo_to_mysql(gene_db_list[i], sql_table_list[i])
--- final_search_ab_local.py
+++ final_search_ab_local.py
@@ -3,6 +3,10 @@ import jieba
 
 
 def search_ab(ab: str, org: str):
+    # 函数说明：
+    # 功能：用jieba把摘要分词得到词列表，遍历词列表，找出其中存在的某物种的基因
+    # 参数：ab：摘要；org：物种名
+    # 返回值：返回找到的基因的列表
     vlist = jieba.lcut(ab)
     res_list = []
     g_db_name = "新物种基因库"
@@ -11,10 +15,28 @@ def search_ab(ab: str, org: str):
         print("物种【{0}】没有对应的基因库".format(org))
         return
     col = g_db.get_collection(org)
-    for v in vlist:
+    for i in range(0, len(vlist)):
+        v = vlist[i]
         if ('a' < v[0] < 'z') or ('A' < v[0] < 'Z'):
             if col.find_one({"gene_name": v}):
-                res_list.append(v)
+                # 找出最近的前一个句号的下标
+                min_idx = i
+                for mi in range(0, i):
+                    if vlist[i - mi] == "。":
+                        min_idx = i - mi
+                # 找出最近的后一个句号的下标
+                max_idx = i
+                for ma in range(i, len(vlist)):
+                    if vlist[ma] == "。":
+                        max_idx = ma
+                # 判断两个句号之间是否有关键词
+                is_gene = False
+                for j in range(min_idx, max_idx + 1):
+                    if vlist[j] in around_check_list:
+                        is_gene = True
+                        break
+                if is_gene:
+                    res_list.append(v)
     return res_list
 
 
@@ -34,6 +56,8 @@ def search_all_ab(ab_source_db_name: str, res_db_name: str):
     else:
         res_db = clint[res_db_name]
 
+    gene_db = clint.get_database("新物种基因库")
+
     for cname in db.list_collection_names():
         col = db.get_collection(cname)
         if cname in g_org_list:
@@ -49,6 +73,8 @@ def search_all_ab(ab_source_db_name: str, res_db_name: str):
         else:
             res_col = res_db[final_org]
 
+        gene_col = gene_db.get_collection(cname)
+
         count = 0
         cursor = col.find(no_cursor_timeout=True)
         for ref in cursor:
@@ -59,11 +85,18 @@ def search_all_ab(ab_source_db_name: str, res_db_name: str):
             glist = search_ab(ab, final_org)
             if type(glist) is list and glist:
                 for g in glist:
+                    g_ref = gene_col.find_one({"gene_name": g})
+                    if g_ref:
+                        anno = g_ref["annotation"]
+                    else:
+                        anno = "Null"
                     rec = {
                         "基因": g,
                         "指标": mea,
                         "物种": final_org,
-                        "标题": title
+                        "标题": title,
+                        "摘要": ab,
+                        "注释": anno
                     }
                     if res_col.find_one(rec):
                         continue
@@ -77,6 +110,7 @@ clint = MongoClient()
 g_org_list = ["绵羊", "山羊", "奶牛", "水牛", "牦牛", "猪", "绿头鸭", "疣鼻栖鸭", "家鸡", "火鸡", "狗", "猫", "马"]
 org_chg_dic = {"犬": "狗"}
 source_db_list = ["新知网摘要库", "新万方摘要库", "新维普摘要库"]
-res_db_list = ["新知网基因库", "新万方基因库", "新维普基因库"]
+res_db_list = ["新知网基因库3", "新万方基因库3", "新维普基因库3"]
+around_check_list = ["基因", "遗传位点"]
 for i in range(0, 3):
     search_all_ab(source_db_list[i], res_db_list[i])
